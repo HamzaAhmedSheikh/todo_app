79cbb194bf1c0eb22a97d94c89e37868c24187f2
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-25 22:10:01 +0500

    styling

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var list = document.getElementById('list')
 
 function addTodo() {
     var todo_item = document.getElementById('todo-item')
+
+    // create li tag with text node
     var li = document.createElement('li')
     var liText = document.createTextNode(todo_item.value)
+        li.appendChild(liText)
 
-    li.appendChild(liText)
+    // create delete button    
+    var delBtn = document.createElement("button")
+
+    
     list.appendChild(li)
+
+    list.setAttribute('class', 'list-group-item');
 
     todo_item.value = ""
 }
